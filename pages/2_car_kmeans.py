53e3e1fa2ccd0b911dc865a17ea559e4acc3e95a
mongamonga1 <ayj120202@gmail.com>
2025-08-04 14:25:16 +0900

    Update 2_car_kmeans.py

--- pages/2_car_kmeans.py
+++ pages/2_car_kmeans.py
@@ -69,8 +69,8 @@ nav = st.navigation({
     "": [
         st.Page("app.py", title="🏠 메인 화면"),
         st.Page("pages/2_car_kmeans.py", title="🚗 차명별 군집분석"),
-        st.Page("pages/3_recommend_system.py", title="🧠 기업 추"),
-        st.Page("pages/4_forest_lstm.py", title="🌲 이상거래 의"),
+        st.Page("pages/4_recommend_system.py", title="🧠 기업 추"),
+        st.Page("pages/5_forest_lstm.py", title="🌲 이상거래 의"),
         st.Page("pages/5_timeseries_analysis.py", title="📈 시세 분석"),
     ]
 })
